46d44016a00c9c6b7ae7055abc3a3140075bfc61
blanq31 <obiwulugodswill@gmail.com>
2023-07-27 22:13:53 +0100

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+
+const About = () => {
+  return (
+    <main className="About">
+        <h2>About</h2>
+        <p style={{ marginTop: "1rem" }}>
+          This blog app is  project in the Learn React tutorial series.
+        </p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/NewPost.jsx
@@ -0,0 +1,30 @@
+
+
+const NewPost = ({ setPostBody, postBody, setPostTitle, postTitle, handleSubmit }) => {
+  return (
+    <main className="NewPost">
+        <h2>NewPost</h2>
+        <form className="newPostForm" onSubmit={handleSubmit}>
+          <label htmlFor="postTitle">Title</label>
+          <input 
+            id="postTitle"
+            type="text"
+            required
+            value={postTitle}
+            onChange={(e) => setPostTitle(e.target.value)}
+          />
+          <label htmlFor="postBody">Post:</label>
+          <textarea
+            id="postBody"
+            required
+            value={postBody}
+            onChange={(e) => setPostBody(e.target.value)}
+          >
+          </textarea>
+          <button type="submit" onClick={handleSubmit}>Submit</button>
+        </form>
+    </main>
+  )
+}
+
+export default NewPost
